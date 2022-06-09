dfd076d0504d6ddbf3250c09a5270588b03f1192
Tejas Narayanan <tejasn100@gmail.com>
2022-06-08 20:49:45 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(name='dll',
       version='0.0.1',
       install_requires=['numpy'],
-      packages=find_packages(),
+      packages=['dll', 'dll.data', 'dll.layers', 'dll.optimizers'],
       include_package_data=True)
