a2987157997fad17c82c8d13a793dd9d6feff7e4
Tejas Narayanan <tejasn100@gmail.com>
2022-06-08 20:55:56 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 
 setup(name='dll',
       version='0.0.1',
+      author='Tejas Narayanan',
+      description='A basic deep learning library written from scratch using Numpy',
+      url='https://github.com/tnarayanan/deep-learning-library',
       install_requires=['numpy'],
-      packages=['dll', 'dll.data', 'dll.layers', 'dll.optimizers'],
-      include_package_data=True)
+      packages=find_packages())
